da45164b6f68244e8ba26f0bf32fbfd59bcb4fc3
m-sarful <isarful823@gmail.com>
2024-03-02 14:31:01 +0600

    get image from the link

--- scripts/index.js
+++ scripts/index.js
@@ -1,36 +1,38 @@
 const loadData = async () => {
-    
-        const res = await fetch('https://openapi.programming-hero.com/api/ai/tool/01');
+        const res = await fetch(`https://openapi.programming-hero.com/api/ai/tools`);
         const data = await res.json();
-        const aiContents = data.data;
-        console.log(aiContents.id);
-        console.log(aiContents);
-        
-        // console.log(typeof aiContents);
-        aiDisplayContents(aiContents);
+        const contents = data.data;
+        aiDisplayContents(contents);
     } 
 
-const aiDisplayContents = aiContents => {
-    // for(const item in aiContents){
-    //     console.log(item)
-    // }
-    const value = Object.values(aiContents);
-    console.log(value);
-    const id = value[6][0].input;
-    const img = value[5][0];
-   const container = document.getElementById('phone-container');
-   const containerDiv = document.createElement('div');
-   container.innerHTML = `
-   <h1>${value[6][0].input}</h1>
-   <img src="${value[5][0]}" alt="">
-   
-   `
+const aiDisplayContents = contents =>{
+    const datas = contents.tools;
+//   console.log(datas);
+  for(const data of datas){
+    const img = data.image;
+    const container = document.getElementById('ai-container');
+   const div = document.createElement('div');
+   div.classList = `card p-4 bg-base-100 shadow-xl`;
+   div.innerHTML = `
+   <figure><img src="${data.image}" alt="Shoes" /></figure>
+   <div class="card-body">
+     <h2 class="card-title">Shoes!</h2>
+     <p>If a dog chews shoes whose shoes does he choose?</p>
+     <div class="card-actions justify-end">
+       <button class="btn btn-primary">Buy Now</button>
+     </div>
+   </div>
+   `;
+   container.appendChild(div);
+    }
+  }
 
 
-    // const id = value[1];
-    console.log(id);
 
-}
+
+
+
+
 
 loadData();
 
